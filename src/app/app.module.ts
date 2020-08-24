f79695e459e48f34c5e65f9d075c162892aadccb
Ankita Shreya <ankita9shreya@gmail.com>
2020-08-24 13:51:58 +0530

    Understanding Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,41 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
+import { UsersComponent } from './users/users.component';
+import { HomeComponent } from './home/home.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { UserComponent } from './users/user/user.component';
+import { Routes, RouterModule } from '@angular/router';
+import { ServersService } from './servers/servers.service';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+const appRoutes: Routes =[
+ { path: '', component: HomeComponent},
+ { path: 'users', component: UsersComponent},
+ { path: 'servers', component: ServersComponent}  
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServersComponent,
+    UsersComponent,
+    HomeComponent,
+    EditServerComponent,
+    ServerComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [ServersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
