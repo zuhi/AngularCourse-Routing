82bdec4803bd4f40b294ed2fe806b97324f5a81c
Ankita Shreya <ankita9shreya@gmail.com>
2020-08-25 13:51:34 +0530

    Snapshot-params doesnot loads the updated data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { HttpClientModule } from '@angular/common/http';
 const appRoutes: Routes =[
  { path: '', component: HomeComponent},
  { path: 'users', component: UsersComponent},
- { path: 'users/:id', component: UserComponent},
+ { path: 'users/:id/:name', component: UserComponent},
  { path: 'servers', component: ServersComponent}  
 ];
 
