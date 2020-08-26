8492d1e060d040715b8934fac42424111b3cf64d
Ankita Shreya <ankita9shreya@gmail.com>
2020-08-26 13:07:47 +0530

    Destroy subscribed observables which you have created

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -22,6 +24,9 @@ export class UserComponent implements OnInit {
         this.user.name = params['name'];
       }
     );
+  }
+  ngOnDestroy(){
+    this.paramSubscription.unsubscribe();
   }
 
 }
