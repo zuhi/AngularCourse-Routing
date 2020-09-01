1cdc57d5201480c2ddefa2832d579862726792f3
Ankita Shreya <ankita9shreya@gmail.com>
2020-09-01 23:01:18 +0530

    Controlling Navigation with canDeactivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,15 +8,16 @@ import { UsersComponent } from './users/users.component';
 import { HomeComponent } from './home/home.component';
 import { ServersComponent } from './servers/servers.component';
 import { AuthGuard } from './auth-guard.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
 
 const appRoutes: Routes =[
   { path: '', component: HomeComponent},
   { path: 'users', component: UsersComponent, children:[
    { path: ':id/:name', component: UserComponent}
   ]},
-  { path: 'servers', canActivateChild: [AuthGuard],component: ServersComponent, children:[
+  { path: 'servers', canActivateChild: [AuthGuard], component: ServersComponent, children:[
   { path: ':id', component: ServerComponent},
-  { path: ':id/edit', component: EditServerComponent}]},
+  { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard]}]},
   { path: 'page-not-found', component: PageNotFoundComponent},
   { path: '**', redirectTo: 'page-not-found'}
  ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth-guard.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
 
 
 
@@ -37,7 +38,7 @@ import { AuthGuard } from './auth-guard.service';
     HttpClientModule,
     AppRoutingModule
   ],
-  providers: [ServersService, AuthService, AuthGuard],
+  providers: [ServersService, AuthService, AuthGuard, CanDeactivateGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
