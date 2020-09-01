d32e7e54bc454d1c2abca9e12ec6b549509f189c
Ankita Shreya <ankita9shreya@gmail.com>
2020-09-01 23:59:00 +0530

    Resolving Dynamic Data with the Resolve Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { ServersComponent } from './servers/servers.component';
 import { AuthGuard } from './auth-guard.service';
 import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
 import { ErrorPageComponent } from './error-page/error-page.component';
+import { ServerResolver } from './servers/server/server-resolver.service';
 
 const appRoutes: Routes =[
   { path: '', component: HomeComponent},
@@ -17,7 +18,7 @@ const appRoutes: Routes =[
    { path: ':id/:name', component: UserComponent}
   ]},
   { path: 'servers', canActivateChild: [AuthGuard], component: ServersComponent, children:[
-  { path: ':id', component: ServerComponent},
+  { path: ':id', component: ServerComponent, resolve: {server: ServerResolver}},
   { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard]}]},
   //{ path: 'page-not-found', component: PageNotFoundComponent},
   { path: 'page-not-found', component: ErrorPageComponent, data: {message: 'Page not found'}},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { AuthService } from './auth.service';
 import { AuthGuard } from './auth-guard.service';
 import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
 import { ErrorPageComponent } from './error-page/error-page.component';
+import { ServerResolver } from './servers/server/server-resolver.service';
 
 
 
@@ -40,7 +41,7 @@ import { ErrorPageComponent } from './error-page/error-page.component';
     HttpClientModule,
     AppRoutingModule
   ],
-  providers: [ServersService, AuthService, AuthGuard, CanDeactivateGuard],
+  providers: [ServersService, AuthService, AuthGuard, CanDeactivateGuard, ServerResolver],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
